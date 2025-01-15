88da659f3cbc7e6d82bf5054cd6ec5b2f70e49ab
Glaudir Schlemper <glaudir@digilab.corp>
2025-01-15 15:25:54 -0300

    jogo finalizado

---
+++ forca.h
@@ -0,0 +1,12 @@
+#define TAMANHO_PALAVRA 20
+
+// declara todas as funcoes
+void abertura();
+void chuta();
+int jachutou(char letra);
+void desenhaforca();
+void escolhepalavra();
+int acertou();
+int enforcou();
+void adicionapalavra();
+int chuteserrados();
